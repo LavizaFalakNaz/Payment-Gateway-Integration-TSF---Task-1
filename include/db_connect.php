5255c806bb9c9ede76fffc0b5058c3d26cfe3cda
Laviza Falak Naz <60230431+LavizaFalakNaz@users.noreply.github.com>
2021-04-10 21:52:51 +0500

    Add files via upload

---
+++ include/db_connect.php
@@ -0,0 +1,10 @@
+<?php 
+
+// Connect with the database 
+$db = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME); 
+ 
+// Display error if failed to connect 
+if ($db->connect_errno) { 
+    echo "Connection to database is failed: ".$db->connect_error;
+    exit();
+}
